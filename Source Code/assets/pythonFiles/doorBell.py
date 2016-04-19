61739535800f1b4af339ac93a6559a4c543e8233
Timbo Cole <timbo_cole@yahoo.co.uk>
2016-04-19 10:48:38 +0100

    Update doorBell.py

--- Source Code/assets/pythonFiles/doorBell.py
+++ Source Code/assets/pythonFiles/doorBell.py
@@ -7,6 +7,9 @@ import RPi.GPIO as GPIO
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "## YOUR TWILLIO SID"
 auth_token  = "## YOUR TWILLIO TOKEN"
+recipient_phoneNo = ""
+twilio_phoneNo =""
+
 client = TwilioRestClient(account_sid, auth_token)
 
 GPIO.setmode(GPIO.BCM)
@@ -16,7 +19,7 @@ while True:
 	if (GPIO.input(22) == True):
 		print "DOORBELL"
 		message = client.messages.create(body="There is a visitor at your door, reply \"OK\" to unlock",
-   		to="## RECIPIENT PHONE NUMBER",    # Replace with your phone number
-    		from_="## YOUR TWILLIO PHONE NUMBER") # Replace with your Twilio number
+   		to=recipient_phoneNo,    # Replace with your phone number
+    		from_=twilio_phoneNo) # Replace with your Twilio number
 		sleep(20)
 	sleep(0.1)
